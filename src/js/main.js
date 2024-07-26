36329d8d1eb67cb783788300ddf68be6cdad475e
Suetone Carneiro <suetoneneto12@gmail.com>
2024-07-25 21:17:52 -0300

    feat: main page

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,21 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const main = document.querySelector('html');
+const botaoInscrucoes = document.getElementById('instrucoes');
+const modal = document.querySelector('dialog');
+const buttonClose = document.querySelector('dialog button');
+
+botaoInscrucoes.onclick = function(){instrucoes()};
+
+function instrucoes(){
+    modal.showModal();
+}
+
+buttonClose.onclick = function(){
+    modal.close();
+}
