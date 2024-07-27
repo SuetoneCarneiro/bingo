b1448b4a1c649dea4b70718a33b1ab13ea1bd235
Suetone Carneiro <suetoneneto12@gmail.com>
2024-07-27 17:21:03 -0300

    feat: criação do sorteio simples, sem salvar nada

--- src/js/jogo.js
+++ src/js/jogo.js
@@ -1,6 +1,6 @@
 
-const botaoJogar = document.getElementById('jogar');
 
+const botaoJogar = document.getElementById('jogar');
 // Abre a página de jogo ao clicar no botão "jogar"
 botaoJogar.onclick = function(){
     const nomeBingo = document.getElementById('nome-bingo').value;
@@ -8,4 +8,19 @@ botaoJogar.onclick = function(){
     // Armazena o valor do nome escolhido no localStorage
     localStorage.setItem('nomeBingo', nomeBingo);
     window.location.href="jogo.html";
+}
+
+
+function sorteio(){
+    //alert('Funcionou!');
+    const numeroMostrado = document.getElementById('numero-sorteado');
+    numeroMostrado.setAttribute('class','border border-primary rounded-circle p-3');
+    
+    const numeroSorteado = Math.floor(Math.random() * 90) + 1; // Gera um número entre 1 e 90
+    numeroMostrado.textContent = numeroSorteado > 9 ? numeroSorteado : '0' + numeroSorteado; // Atualiza o texto do h1
+    // a função "includes()" checa se o elemento está no array. Retorna true or false
+}
+
+function teste(){
+    alert('Teste');
 }
