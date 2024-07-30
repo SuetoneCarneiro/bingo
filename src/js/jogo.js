2c287c04c460a524015159f0313d9d22291bc7df
Suetone Carneiro <suetoneneto12@gmail.com>
2024-07-30 16:14:28 -0300

    simplying the project

--- src/js/jogo.js
+++ src/js/jogo.js
@@ -2,25 +2,28 @@
 
 const botaoJogar = document.getElementById('jogar');
 // Abre a página de jogo ao clicar no botão "jogar"
-botaoJogar.onclick = function(){
+botaoJogar.onclick = function () {
     const nomeBingo = document.getElementById('nome-bingo').value;
 
     // Armazena o valor do nome escolhido no localStorage
     localStorage.setItem('nomeBingo', nomeBingo);
-    window.location.href="jogo.html";
+    window.location.href = "jogo.html";
 }
 
-
-function sorteio(){
-    //alert('Funcionou!');
-    const numeroMostrado = document.getElementById('numero-sorteado');
-    numeroMostrado.setAttribute('class','border border-primary rounded-circle p-3');
+function sorteio() {
     
-    const numeroSorteado = Math.floor(Math.random() * 90) + 1; // Gera um número entre 1 e 90
-    numeroMostrado.textContent = numeroSorteado > 9 ? numeroSorteado : '0' + numeroSorteado; // Atualiza o texto do h1
-    // a função "includes()" checa se o elemento está no array. Retorna true or false
-}
+    const numeroMostrado = document.getElementById('numero-sorteado');
+    numeroMostrado.setAttribute('class', 'border border-primary rounded-circle p-3');
+    console.log(numerosSorteados)
+    let numeroSorteado = Math.floor(Math.random() * 90) + 1; // Gera um número entre 1 e 90
+    // Lógica do sorteio
+    if(numerosSorteados.includes(numeroSorteado)){// a função "includes()" checa se o elemento está no array. Retorna true or false
+        while(numerosSorteados.includes(numeroSorteado)) numeroSorteado = Math.floor(Math.random() * 90) + 1;
+    }
 
-function teste(){
+    numeroMostrado.textContent = numeroSorteado > 9 ? numeroSorteado : '0' + numeroSorteado; // Atualiza o texto do h1
+    numerosSorteados.push(numeroSorteado);
+}
+function teste() {
     alert('Teste');
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,4 @@
-// Import our custom CSS
-import '../scss/styles.scss'
 
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-const main = document.querySelector('html');
 const botaoInscrucoes = document.getElementById('instrucoes');
 const modal = document.querySelector('dialog');
 const buttonClose = document.querySelector('dialog button');
