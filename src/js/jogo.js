d9a9847d3e538f59b0c1e68086f782e2eddf0a7c
Suetone Carneiro <suetoneneto12@gmail.com>
2024-08-03 20:38:00 -0300

    Logica do jogo, animacao e ajustes nos numeros mostrados na tela

--- src/js/jogo.js
+++ src/js/jogo.js
@@ -11,18 +11,51 @@ botaoJogar.onclick = function () {
 }
 
 function sorteio() {
-    
     const numeroMostrado = document.getElementById('numero-sorteado');
+    const numeroAnterior = document.getElementById('numero-anterior');
     numeroMostrado.setAttribute('class', 'border border-primary rounded-circle p-3');
-    console.log(numerosSorteados)
-    let numeroSorteado = Math.floor(Math.random() * 90) + 1; // Gera um número entre 1 e 90
-    // Lógica do sorteio
-    if(numerosSorteados.includes(numeroSorteado)){// a função "includes()" checa se o elemento está no array. Retorna true or false
+    // Lógica da animação do sorteio
+    let animacao;
+    animacao = setInterval(() => {
+        numeroSorteado = Math.floor(Math.random() * 90) + 1;
+        if(numerosSorteados.includes(numeroSorteado)){// a função "includes()" checa se o elemento está no array. Retorna true or false
         while(numerosSorteados.includes(numeroSorteado)) numeroSorteado = Math.floor(Math.random() * 90) + 1;
     }
+        
+        numeroMostrado.textContent = numeroSorteado > 9 ? numeroSorteado : '0' + numeroSorteado; // Atualiza o texto do h1
 
-    numeroMostrado.textContent = numeroSorteado > 9 ? numeroSorteado : '0' + numeroSorteado; // Atualiza o texto do h1
+    }, 25);
+    setTimeout(() => {clearInterval(animacao)}, 500);
+
     numerosSorteados.push(numeroSorteado);
+    
+    if(numerosSorteados.length > 1){
+        if(numeroSorteado > 9) numeroAnterior.textContent = 'Último número sorteado: ' + numerosSorteados[numerosSorteados.length-1];
+        else numeroAnterior.textContent = 'Último número sorteado: 0' + numerosSorteados[numerosSorteados.length-1]; 
+    }
+    console.log(numerosSorteados);
+    
+}
+
+function showNumbers(){
+    const modal = document.querySelector('.pagina-sorteados');
+    const closeModal = document.querySelector('.botao-modal');
+    const listaNumeros = document.querySelector('#lista-numeros');
+    modal.showModal();
+    let show = '';
+    numerosSorteados.map((number) => {
+        if(number != 0){
+            if(number > 9) show += `${number}`;
+            else show += `0${number}`;
+            let lastElement = numerosSorteados.slice(-1);
+            if(number != lastElement) show += ', ';
+        }
+        
+    }).join('');
+    
+    listaNumeros.textContent = show;
+    closeModal.addEventListener('click', function(){modal.close();})
+    
 }
 function teste() {
     alert('Teste');
